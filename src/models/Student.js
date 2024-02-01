a585398ec1072300be3e92d4384e76e9615f2ce9
Marcos Filipe Capella <marcoscapella@outlook.com>
2024-02-01 11:24:24 -0300

    Implemented validators to Student and Teacher schemas

--- src/models/Student.js
+++ src/models/Student.js
@@ -1,17 +1,50 @@
 import mongoose from "mongoose";
+import { validate } from "gerador-validador-cpf";
 
 const studentSchema = new mongoose.Schema(
   {
     id: { type: mongoose.Types.ObjectId },
-    name: { type: String, required: true },
-    cpf: { type: String, required: true },
-    phoneNumber: { type: String },
+    name: { type: String, required: true, maxLength: [150, "The name cannot be longer than 150 characters."] },
+    cpf: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: {
+        validator: function (value) {
+          return validate(value);
+        },
+        message: (props) =>
+          `${props.value} is not a valid CPF / não é um CPF válido!`,
+      },
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+      validate: {
+        validator: function (value) {
+          return /^[0-9]{10,11}$/.test(value);
+        },
+        message: (props) =>
+          `${props.value} não é um número de telefone válido! O formato deve conter apenas dígitos e ter entre 10 e 11 caracteres. / not a valid Brazilian phone number! The format must contain only digits between 10 and 11 characters.`,
+      },
+    },
     address: { type: String },
-    dateOfBirth: { type: Date },
-    email: { type: String },
+    dateOfBirth: {
+      type: Date,
+      validate: {
+        validator: function (value) {
+          return value instanceof Date && !isNaN(value);
+        },
+        message: "Invalid date of birth",
+      },
+    },
+    email: { type: String, required: true, match: /^\S+@\S+\.\S+$/ },
     monthlyTuition: { type: Number },
-    dueDate: { type: Number },
-    group: { type: mongoose.Types.ObjectId, ref: "groups" }
+    dueDate: { 
+      type: Number, 
+      min: [1, "Due date must be a number within a month."], 
+      max: [31, "Due date must be a number within a month."] },
+    group: { type: mongoose.Types.ObjectId, ref: "groups" },
   },
   { versionKey: false }
 );
--- src/models/Teacher.js
+++ src/models/Teacher.js
@@ -1,13 +1,43 @@
 import mongoose from "mongoose";
+import { validate } from "gerador-validador-cpf";
 
 const teacherSchema = new mongoose.Schema(
   {
     id: { type: mongoose.Types.ObjectId },
-    name: { type: String, required: true },
-    email: { type: String, required: true },
-    phoneNumber: { type: String },
-    cpf: { type: String },
-    dateOfBirth: { type: Date },
+    name: { type: String, required: true, maxLength: 100 },
+    email: { type: String, required: true, match: /^\S+@\S+\.\S+$/  },
+    phoneNumber: {
+      type: String,
+      required: true,
+      validate: {
+        validator: function (value) {
+          return /^[0-9]{10,11}$/.test(value);
+        },
+        message: (props) =>
+          `${props.value} não é um número de telefone válido! O formato deve conter apenas dígitos e ter entre 10 e 11 caracteres. / not a valid Brazilian phone number! The format must contain only digits between 10 and 11 characters.`,
+      },
+    },
+    cpf: {
+      type: String,
+      required: true,
+      unique: true,
+      validate: {
+        validator: function (value) {
+          return validate(value);
+        },
+        message: (props) =>
+          `${props.value} is not a valid CPF / não é um CPF válido!`,
+      },
+    },
+    dateOfBirth: {
+      type: Date,
+      validate: {
+        validator: function (value) {
+          return value instanceof Date && !isNaN(value);
+        },
+        message: "Invalid date of birth",
+      },
+    },
     address: { type: String },
   },
   { versionKey: false }
